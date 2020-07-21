77f8ff36bfbd8396c233d2000707545df1a1c9d5
박수정 <crystalpark@sqisoft.com>
2020-07-21 09:09:28 +0900

    commit

---
+++ scripts/deploy.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+BUILD_PATH=$(ls /home/ec2-user/build/config/*.jar)
+JAR_NAME=$(basename $BUILD_PATH)
+echo "> build 파일명 : $JAR_NAME"
+
+DEPLOY_PATH=/home/ec2-user/docker/config/
+CP_JAR_PATH=$DEPLOY_PATH$JAR_NAME
+
+if [ ! -d $DEPLOY_PATH ] ; then
+  echo "> mkdir $DEPLOY_PATH"
+  sudo mkdir $DEPLOY_PATH
+fi
+
+if [ -f $CP_JAR_PATH ] ; then
+  echo "> remove $CP_JAR_PATH"
+  sudo rm -f $CP_JAR_PATH
+fi
+
+echo "> copy $BUILD_PATH >> $CP_JAR_PATH"
+sudo cp $BUILD_PATH $CP_JAR_PATH
+
+echo "> cd $DOCKER_PATH"
+DOCKER_PATH=/home/ec2-user/docker/
+cd $DOCKER_PATH
+
+SHELL_PATH=`pwd`
+echo $SHELL_PATH
+
+TOMCAT_SERVICE_NAME=config
+sudo docker-compose up -d ${TOMCAT_SERVICE_NAME}
---
+++ src/test/java/com/sqisoft/configserver/ConfigserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sqisoft.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
