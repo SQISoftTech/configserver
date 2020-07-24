c2e02d902bead781fa5f129422062dd4e1bbce87
박수정 <crystalpark@sqisoft.com>
2020-07-24 17:41:10 +0900

    도커 이미지 빌드 후 기존 이미지 정리 명령어 추가

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,16 +6,6 @@ echo "> build 파일명 : $JAR_NAME"
 DEPLOY_PATH=/home/ec2-user/docker/config/
 CP_JAR_PATH=$DEPLOY_PATH$JAR_NAME
 
-if [ ! -d $DEPLOY_PATH ] ; then
-  echo "> mkdir $DEPLOY_PATH"
-  sudo mkdir $DEPLOY_PATH
-fi
-
-if [ -f $CP_JAR_PATH ] ; then
-  echo "> remove $CP_JAR_PATH"
-  sudo rm -f $CP_JAR_PATH
-fi
-
 echo "> copy $BUILD_PATH >> $CP_JAR_PATH"
 sudo cp $BUILD_PATH $CP_JAR_PATH
 
@@ -26,5 +16,7 @@ cd $DOCKER_PATH
 SHELL_PATH=`pwd`
 echo $SHELL_PATH
 
-TOMCAT_SERVICE_NAME=config
-sudo docker-compose up -d ${TOMCAT_SERVICE_NAME}
+DOCKER_SERVICE_NAME=config
+sudo docker-compose up -d --build ${DOCKER_SERVICE_NAME}
+
+sudo docker image prune -f
