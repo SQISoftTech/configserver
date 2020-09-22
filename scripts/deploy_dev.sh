659737ce16264d0154eacf1343748f9a1348edb1
이서주 <potential565@sqisoft.com>
2020-09-22 13:50:33 +0900

    prd 프로파일 추가

--- scripts/deploy_dev.sh
+++ scripts/deploy_dev.sh
@@ -1,28 +1,27 @@
 #!/bin/sh
 #
 #chkconfig:234 90 90
-#descript: psapisserver service
-#processname : psapiserver
+#descript: configserver service
+#processname : configserver
 #
 JAVA_HOME=/usr/lib/jvm/jre
 SERVICE_NAME=configserver
 PATH_TO_JAR=/usr/share/api_server/configserver/configserver-0.0.1-SNAPSHOT.jar
 PID_PATH_NAME=/tmp/configserver-pid
 JAVA_OPTS="-server -Dspring.profiles.active=dev"
-PATH=$PATH:$JVA_HOME/bin
+PATH=$PATH:JAVA_HOME/bin
+PID=`ps aux | grep configserver | grep -v grep | awk '{print $2}'`
 
-        echo "Starting $SERVICE_NAME ..."
-        echo "$PID_PATH_NAME"
-        if [ ! -f $PID_PATH_NAME ]; then
-            sudo nohup java $JAVA_OPTS -jar $PATH_TO_JAR /tmp 2>> /dev/null >> /dev/null &
-            echo $! > $PID_PATH_NAME
-            echo "$SERVICE_NAME started ..."
-        else
-             echo "$SERVICE_NAME stoping ..."
-             sudo ps -elf | grep $PATH_TO_JAR | grep -v grep |  awk '{print $4}' | while read line; do kill $line; done
-             echo "$SERVICE_NAME stopped ..."
-             sudo rm -f $PID_PATH_NAME
-             sudo nohup java $JAVA_OPTS -jar $PATH_TO_JAR /tmp 2>> /dev/null >> /dev/null &
-             echo $! > $PID_PATH_NAME
-             echo "$SERVICE_NAME started ..."
-        fi
+echo "$PID"
+            if [[ "" !=  "$PID" ]]; then
+              echo "killing $PID"
+              kill -9 $PID
+              echo "$SERVICE_NAME stopped ..."
+                  java $JAVA_OPTS -jar $PATH_TO_JAR /tmp 2>> /dev/null >> /dev/null &
+              echo $! > $PID_PATH_NAME
+              echo "$SERVICE_NAME started ..."
+           else
+                java $JAVA_OPTS -jar $PATH_TO_JAR /tmp 2>> /dev/null >> /dev/null &
+              echo $! > $PID_PATH_NAME
+              echo "$SERVICE_NAME started ..."
+           fi
