c7af072cc0dfd056f03d3e1eb9aadead359eea98
이서주 <potential565@sqisoft.com>
2020-09-24 10:28:00 +0900

    deploy_dev ,op 쉘스크립트 파일 커밋

--- scripts/deploy_op.sh
+++ scripts/deploy_op.sh
@@ -12,16 +12,26 @@ JAVA_OPTS="-server -Dspring.profiles.active=op"
 PATH=$PATH:JAVA_HOME/bin
 PID=`ps aux | grep configserver | grep -v grep | awk '{print $2}'`
 
+BUILD_PATH=$(ls /home/ec2-user/build/configserver/*.jar)
+JAR_NAME=$(basename $BUILD_PATH)
+echo "> build 파일명 : $JAR_NAME"
+
+DEPLOY_PATH=/home/ec2-user/deploy/configserver/
+CP_JAR_PATH=$DEPLOY_PATH$JAR_NAME
+
+echo "> copy $BUILD_PATH >> $CP_JAR_PATH"
+sudo cp $BUILD_PATH $CP_JAR_PATH
+
 echo "$PID"
             if [[ "" !=  "$PID" ]]; then
               echo "killing $PID"
               kill -9 $PID
               echo "$SERVICE_NAME stopped ..."
-                  java $JAVA_OPTS -jar $PATH_TO_JAR /tmp 2>> /dev/null >> /dev/null &
+                  java $JAVA_OPTS -jar $CP_JAR_PATH /tmp 2>> /dev/null >> /dev/null &
               echo $! > $PID_PATH_NAME
               echo "$SERVICE_NAME started ..."
            else
-                java $JAVA_OPTS -jar $PATH_TO_JAR /tmp 2>> /dev/null >> /dev/null &
+                java $JAVA_OPTS -jar $CP_JAR_PATH /tmp 2>> /dev/null >> /dev/null &
               echo $! > $PID_PATH_NAME
               echo "$SERVICE_NAME started ..."
            fi
